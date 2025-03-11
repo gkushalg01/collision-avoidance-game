a17babddd6b267d6caf18cd2b2fccb2898d4e9bf
Kushal Ghosh <gkushalg01@gmail.com>
2025-03-12 02:32:10 +0530

    Add: Color switching between obstacle and waypoint works as intended

--- map_creator.gd
+++ map_creator.gd
@@ -11,22 +11,17 @@ func _ready() -> void:
 	_astar2D.clear() # Replace with function body.
 
 func _input(event: InputEvent) -> void:
-	#if(event.is_action("mouseLeft")):
 	if(event.is_action("mouseLeft") && event.is_pressed()):
 		var currentPos: = get_global_mouse_position()
 	
-		if(_selectedTileType == TileType.WAYPOINT):
-			createWaypoint(currentPos)
-		#elif(_selectedTileType == TileType.OBSTACLE):
-			#createObstacle(currentPos)
-			#print("created obs")
-			
-	elif(event.is_action_pressed("mouseRight")):
-		_astar2D.set_point_disabled(getTileId())
+		createPoint(currentPos)
+		
+	# To-Do Later
+	#elif(event.is_action_pressed("mouseRight")):
+		#_astar2D.set_point_disabled(getTileId())
 		
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
@@ -43,15 +38,15 @@ func getTileId() -> int:
 func _draw() -> void:
 	for i in range(0, _astar2D.get_point_count()):
 		var perc = float(i)/_astar2D.get_point_count()
-		draw_circle(_astar2D.get_point_position(i), 15, Color(perc, 1, 1-perc))
+		var wp_color := Color.ROSY_BROWN if _astar2D.is_point_disabled(i) else Color.CHARTREUSE
+		draw_circle(_astar2D.get_point_position(i), 15, wp_color)
 		draw_string(ThemeDB.fallback_font, _astar2D.get_point_position(i), str(i), HORIZONTAL_ALIGNMENT_CENTER, -1, 16, Color.BLACK)
 		#draw_rect(Rect2(_astar2D.get_point_position(i) * 32 + Vector2(50, 51), Vector2(50, 50)-Vector2.ONE), Color(perc, 1, 1-perc))
 
 
-func createWaypoint(currentPos: Vector2) -> void:
+func createPoint(currentPos: Vector2) -> void:
 	_astar2D.add_point(getTileId(), currentPos)
+	if(_selectedTileType == TileType.OBSTACLE):
+		_astar2D.set_point_disabled(_tileId)
+		
 	queue_redraw()
-
-
-func createObstacle(currentPos: Vector2) -> void:
-	_astar2D.add_point(getTileId(), currentPos)
