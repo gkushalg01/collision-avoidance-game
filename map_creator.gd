3ecdbc2d313025d2e32bdfbf89dbac1f9b92409d
Kushal Ghosh <gkushalg01@gmail.com>
2025-03-14 15:56:13 +0530

    Add: 'Back' Button in Map Creator

--- map_creator.gd
+++ map_creator.gd
@@ -147,3 +147,7 @@ func connectSelectedWaypoints(connectPoints : bool = true) -> void:
 		prev_point = curr_point
 	_selectedWaypoints.clear()
 	queue_redraw()
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_game.tscn")
